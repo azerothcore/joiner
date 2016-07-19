551330550ecc18dbed424f77e99eaa5a1ca45478
Yehonal <hw.2@hotmail.it>
2016-07-19 08:59:26 +0200

    some fixes

--- joiner.sh
+++ joiner.sh
@@ -3,7 +3,7 @@
 #
 
 PATH_JOINER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PATH_ROOT=$(reallink -f $PATH_JOINER)
+PATH_MODULES=$(readlink -f $PATH_JOINER"/../../")
 
 PARAMS="$@"
 
@@ -16,14 +16,14 @@ function Joiner:add_repo() {
     name=$2
     branch=$3
     basedir=$4
-    path=$PATH_ROOT/$basedir/$name
+    path=$PATH_MODULES/$basedir/$name
     
     ([ -e $path/.git/ ] && git --git-dir=$path/.git/ rev-parse && git --git-dir=$path/.git/ pull origin $branch) || git clone $url/$name.git -b $branch $path 
 	[ -f $path/install.sh ] && bash $path/install.sh $PARAMS
 }
 
 function Joiner:add_file() {
-    mkdir -p $PATH_ROOT/"$(dirname $2)"
-    [ ! -e $PATH_ROOT/$2 ] && curl -o $PATH_ROOT/$2 $1
+    mkdir -p $PATH_MODULES/"$(dirname $2)"
+    [ ! -e $PATH_MODULES/$2 ] && curl -o $PATH_MODULES/$2 $1
 }
 
