94e47c332fa5ed3861d7b13815b031114ea6c39a
Yehonal <yehonal.azeroth@gmail.com>
2016-08-23 14:43:20 +0200

    Implemented add_git_submodule

--- joiner.sh
+++ joiner.sh
@@ -16,10 +16,40 @@ function Joiner:add_repo() {
     name=$2
     branch=$3
     basedir=$4
+
     path=$PATH_MODULES/$basedir/$name
     
-    ([ -e $path/.git/ ] && git --git-dir=$path/.git/ rev-parse && git --git-dir=$path/.git/ pull origin $branch) || git clone $url -c advice.detachedHead=0 -b $branch $path 
-	[ -f $path/install.sh ] && bash $path/install.sh $PARAMS
+    if [ -e $path/.git/ ]; then
+        # if exists , update
+        git --git-dir=$path/.git/ rev-parse && git --git-dir=$path/.git/ pull origin $branch
+    else
+        # otherwise clone
+        git clone $url -c advice.detachedHead=0 -b $branch $path 
+    fi
+
+    [ -f $path/install.sh ] && bash $path/install.sh $PARAMS
+}
+
+function Joiner:add_git_submodule() {
+    url=$1
+    name=$2
+    branch=$3
+    basedir=$4
+
+    path=$PATH_MODULES/$basedir/$name
+    cur_git_path=$(git rev-parse --show-toplevel)"/"
+    rel_path=${path#$cur_git_path}
+
+    if [ -e $path/ ]; then
+        # if exists , update
+        git submodule update --init $rel_path
+    else
+        # otherwise add
+        git submodule add -b $branch $url $rel_path 
+        git submodule update --init $rel_path
+    fi
+
+    [ -f $path/install.sh ] && bash $path/install.sh $PARAMS
 }
 
 function Joiner:add_file() {
