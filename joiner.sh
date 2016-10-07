c2bb73f4f15de17085e52fd03d23784299d7f0e7
Yehonal <yehonal.azeroth@gmail.com>
2016-10-07 16:43:41 +0200

    better parameters support and namespaced variables

--- joiner.sh
+++ joiner.sh
@@ -1,11 +1,17 @@
+#!/usr/bin/env bash
+
+#
+# bash >= 4.x required
+#
+
 #
 # DEFINES
 #
 
-PATH_JOINER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PATH_MODULES=$(readlink -f $PATH_JOINER"/../../")
+J_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+J_PATH_MODULES=$(readlink -f $PATH_JOINER"/../../")
 
-PARAMS="$@"
+J_PARAMS="$@"
 
 #
 # JOINER FUNCTIONS
@@ -17,17 +23,17 @@ function Joiner:add_repo() {
     branch=$3
     basedir=$4
 
-    path=$PATH_MODULES/$basedir/$name
-    
+    path=$J_PATH_MODULES/$basedir/$name
+
     if [ -e $path/.git/ ]; then
         # if exists , update
         git --git-dir=$path/.git/ rev-parse && git --git-dir=$path/.git/ pull origin $branch
     else
         # otherwise clone
-        git clone $url -c advice.detachedHead=0 -b $branch $path 
+        git clone $url -c advice.detachedHead=0 -b $branch $path
     fi
 
-    [ -f $path/install.sh ] && bash $path/install.sh $PARAMS
+    [ -f $path/install.sh ] && bash $path/install.sh $J_PARAMS
 }
 
 function Joiner:add_git_submodule() {
@@ -36,7 +42,7 @@ function Joiner:add_git_submodule() {
     branch=$3
     basedir=$4
 
-    path=$PATH_MODULES/$basedir/$name
+    path=$J_PATH_MODULES/$basedir/$name
     cur_git_path=$(git rev-parse --show-toplevel)"/"
     rel_path=${path#$cur_git_path}
 
@@ -45,15 +51,46 @@ function Joiner:add_git_submodule() {
         git submodule update --init $rel_path
     else
         # otherwise add
-        git submodule add -b $branch $url $rel_path 
+        git submodule add -b $branch $url $rel_path
         git submodule update --init $rel_path
     fi
 
-    [ -f $path/install.sh ] && bash $path/install.sh $PARAMS
+    [ -f $path/install.sh ] && bash $path/install.sh $J_PARAMS
 }
 
 function Joiner:add_file() {
-    mkdir -p $PATH_MODULES/"$(dirname $2)"
-    [ ! -e $PATH_MODULES/$2 ] && curl -o $PATH_MODULES/$2 $1
+    mkdir -p $J_PATH_MODULES/"$(dirname $2)"
+    [ ! -e $J_PATH_MODULES/$2 ] && curl -o $J_PATH_MODULES/$2 $1
 }
+
+function Joiner:with_dev() {
+    if [ "${J_OPT[dev]}" = true ]
+        return 1;
+    else
+        return 0;
+    fi
+}
+
+#
+# Parsing parameters
+#
+
+declare -A J_OPT;
+
+for i in "$@"
+do
+case $i in
+    -e=*|--extras=*)
+		J_OPT[extra]="${i#*=}"
+		shift
+    ;;
+    --dev|-d)
+		J_OPT[dev]=true
+		shift
+    ;;
+    *)
+        # unknown option
+    ;;
+esac
+done
 
